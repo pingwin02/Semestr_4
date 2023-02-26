64a0baed4804de73c1cd90461eb4de25bb65648a
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2023-02-26 21:13:14 +0100

    Added first laboratory

---
+++ Lab1/Comparer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab1
+{
+    public class Comparer : IComparer<string>
+    {
+        public int Compare(string? a, string? b)
+        {
+            if (a == null || b == null)
+            {
+                throw new Exception("Jeden z lancuchow pusty!");
+            }
+
+            if (a.Length > b.Length)
+            {
+                return 1;
+            }
+            else if (a.Length < b.Length)
+            {
+                return -1;
+            }
+            else
+            {
+                return a.CompareTo(b);
+            }
+
+        }
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Lab1
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0];
+            PrintRecursiveFiles(path, 0);
+            Console.WriteLine($"\nNajstarszy plik: {new DirectoryInfo(path).GetOldestDate()}\n");
+            CreateCollection(new DirectoryInfo(path));
+        }
+
+        static void PrintRecursiveFiles(string path, int depth)
+        {
+            string[] files = Directory.GetFiles(path);
+            string[] dirs = Directory.GetDirectories(path);
+
+            Console.WriteLine($"{path.Split("\\").Last()} ({files.Length + dirs.Length}) {new FileInfo(path).GetRahs()}");
+
+            foreach (string dir in dirs)
+            {
+                for (int i = 0; i <= depth; i++)
+                {
+                    Console.Write("\t");
+                }
+                PrintRecursiveFiles(dir, depth + 1);
+            }
+
+            foreach (string file in files)
+            {
+                for (int i = 0; i <= depth; i++)
+                {
+                    Console.Write("\t");
+                }
+                Console.WriteLine($"{file.Split("\\").Last()} {new FileInfo(file).Length} bajtow  {new FileInfo(file).GetRahs()}");
+            }
+
+        }
+
+        static DateTime GetOldestDate(this DirectoryInfo directory)
+        {
+            DateTime oldestDate = DateTime.Now;
+
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                if (file.CreationTime < oldestDate)
+                {
+                    oldestDate = file.CreationTime;
+                }
+            }
+
+            foreach (DirectoryInfo dir in directory.GetDirectories())
+            {
+                DateTime time = GetOldestDate(dir);
+                if (time < oldestDate)
+                {
+                    oldestDate = time;
+                }
+            }
+
+            return oldestDate;
+        }
+
+        static string GetRahs(this FileSystemInfo fsi)
+        {
+            string rahs = "";
+
+            FileAttributes attributes = fsi.Attributes;
+
+            if ((FileAttributes.ReadOnly & attributes) == FileAttributes.ReadOnly)
+            {
+                rahs += 'r';
+            }
+            else
+            {
+                rahs += '-';
+            }
+            if ((FileAttributes.Archive & attributes) == FileAttributes.Archive)
+            {
+                rahs += 'a';
+            }
+            else
+            {
+                rahs += '-';
+            }
+            if ((FileAttributes.Hidden & attributes) == FileAttributes.Hidden)
+            {
+                rahs += 'h';
+            }
+            else
+            {
+                rahs += '-';
+            }
+            if ((FileAttributes.System & attributes) == FileAttributes.System)
+            {
+                rahs += 's';
+            }
+            else
+            {
+                rahs += '-';
+            }
+
+            return rahs;
+        }
+
+        static void CreateCollection(DirectoryInfo path)
+        {
+            SortedDictionary<string, int> collection = new SortedDictionary<string, int>(new Comparer());
+
+            foreach (DirectoryInfo dir in path.GetDirectories())
+            {
+                collection.Add(dir.Name, dir.GetFiles().Length + dir.GetDirectories().Length);
+            }
+
+            foreach (FileInfo file in path.GetFiles())
+            {
+                collection.Add(file.Name, (int)file.Length);
+            }
+
+            foreach (var file in collection)
+            {
+                Console.WriteLine($"{file.Key} -> {file.Value}");
+            }
+
+        }
+
+    }
+}
