d2b95ab5b10ad06a0523d87851a1bcc4bb768d29
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2023-03-08 11:21:59 +0100

    Added lab code

--- Lab1/for_students.py
+++ Lab1/for_students.py
@@ -22,76 +22,64 @@ x_train = train_data['Weight'].to_numpy()
 y_test = test_data['MPG'].to_numpy()
 x_test = test_data['Weight'].to_numpy()
 
-# calculate closed-form solution
+x_train_copy = x_train
+y_train_copy = y_train
+
+
+# standarization
+def standarize(n, train):
+    return (n - np.average(train)) / np.std(train)
+
+
+x_train = standarize(x_train, x_train)
 x_train = np.c_[np.ones(len(x_train)), x_train]
+
+y_train = standarize(y_train, y_train)
+x_test = standarize(x_test, x_train_copy)
+y_test = standarize(y_test, y_train_copy)
+
+# calculate closed-form solution
 theta_best = np.linalg.inv(x_train.T.dot(x_train)).dot(x_train.T).dot(y_train)
 print("Theta regression:", theta_best)
 
+
+def MSE(theta, x, y):
+    _sum = 0
+    for _i in range(len(x)):
+        _sum += ((theta[1] * x[_i] + theta[0] - y[_i]) ** 2) / len(x)
+    return _sum
+
+
+def plot(theta, _x, _y):
+    x = np.linspace(min(_x), max(_x), 100)
+    y = float(theta[0]) + float(theta[1]) * x
+    plt.plot(x, y)
+    plt.scatter(_x, _y)
+    plt.xlabel('Weight')
+    plt.ylabel('MPG')
+    plt.show()
+
+
 # calculate error
-RSS = 0
-for i in range(len(x_test)):
-    RSS += (theta_best[1] * x_test[i] + theta_best[0] - y_test[i]) ** 2
-MSE = RSS / len(x_test)
-print('MSE:', MSE)
+print('MSE:', MSE(theta_best, x_test, y_test))
 
 # plot the regression line
-x = np.linspace(min(x_test), max(x_test), 100)
-y = float(theta_best[0]) + float(theta_best[1]) * x
-plt.plot(x, y)
-plt.scatter(x_test, y_test)
-plt.xlabel('Weight')
-plt.ylabel('MPG')
-plt.show()
-
-# get the columns
-y_train = train_data['MPG'].to_numpy()
-x_train = train_data['Weight'].to_numpy()
-
-y_test = test_data['MPG'].to_numpy()
-x_test = test_data['Weight'].to_numpy()
-
-# standarization
-X_avg = np.average(x_train)
-X_war = np.std(x_train)
-x_train = (x_train - X_avg) / X_war
-x_train = np.c_[np.ones(len(x_train)), x_train]
-
-Y_avg = np.average(y_train)
-Y_war = np.std(y_train)
-y_train = (y_train - Y_avg) / Y_war
-
-X_avg = np.average(x_test)
-X_war = np.std(x_test)
-x_test = (x_test - X_avg) / X_war
-
-Y_avg = np.average(y_test)
-Y_war = np.std(y_test)
-y_test = (y_test - Y_avg) / Y_war
+plot(theta_best, x_test, y_test)
 
 # calculate theta using Batch Gradient Descent
 y_train = y_train.reshape(len(y_train), 1)
 theta_best = np.random.rand(2, 1)
 learning_rate = 0.01
-iterations = 100
+iterations = 1000
 
 for i in range(iterations):
     gradient = 2 / len(x_train) * x_train.T.dot(x_train.dot(theta_best) - y_train)
     theta_best = theta_best - learning_rate * gradient
 
-print("Theta gradient:", theta_best)
+print("Theta gradient:", theta_best.T)
 
 #  calculate error
-RSS = 0
-for i in range(len(x_test)):
-    RSS += (theta_best[1] * x_test[i] + theta_best[0] - y_test[i]) ** 2
-MSE = RSS / len(x_test)
-print('MSE:', MSE)
+print('MSE:', MSE(theta_best, x_test, y_test))
 
 # plot the regression line
-x = np.linspace(min(x_test), max(x_test), 100)
-y = float(theta_best[0]) + float(theta_best[1]) * x
-plt.plot(x, y)
-plt.scatter(x_test, y_test)
-plt.xlabel('Weight')
-plt.ylabel('MPG')
-plt.show()
+plot(theta_best, x_test, y_test)
