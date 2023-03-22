65327342fbaaf82c0798b2775568799c9677fe3c
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2023-03-22 22:23:42 +0100

    Changed some algorithm

--- Proj1/functions.py
+++ Proj1/functions.py
@@ -53,19 +53,42 @@ def plotSIGNALMACD(data):
 
 def simulateSimple(dataset, macd, signal):
     cash = 1000
+    actions = 0
+    cash_history = []
+    actions_history = []
+    cash_history.append(cash)
+    actions_history.append(actions)
 
     data = dataset['Close']
 
     print(f'Początkowa ilość pieniędzy: {cash} PLN')
 
     for i in range(35, len(data)):
-        if macd[i] > signal[i] and macd[i - 1] < signal[i - 1]:
-            cash -= data[i]
+        if macd[i] > signal[i] and macd[i - 1] < signal[i - 1] and cash > 0:
+            actions = int(cash / data[i])
+            cash -= actions * data[i]
             print("Dzień: ", dataset.index[i], end=' ')
-            print(f'Kupiłem akcje za {data[i]} PLN')
-        elif macd[i] < signal[i] and macd[i - 1] > signal[i - 1]:
-            cash += data[i]
+            print(f'Kupiłem akcje za {actions * data[i]} PLN.')
+        elif macd[i] < signal[i] and macd[i - 1] > signal[i - 1] and actions > 0:
             print("Dzień: ", dataset.index[i], end=' ')
-            print(f'Sprzedałem akcje za {data[i]} PLN')
+            print(f'Sprzedałem akcje za {actions * data[i]} PLN')
+            cash += actions * data[i]
+            actions = 0
+        cash_history.append(cash)
+        actions_history.append(actions)
 
-    print(f'Zysk: {cash - 1000} PLN')
+    print(f'Zysk: {cash - 1000} PLN. Pozostało akcji: {actions} sztuk.')
+    print(f'Sprzedaję resztę akcji za {actions * data[len(data) - 1]} PLN.')
+    cash += actions * data[len(data) - 1]
+    print(f'Zysk całkowity: {cash - 1000} PLN.')
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(cash_history, color='blue', label='Pieniądze')
+    plt.plot(actions_history, color='red', label='Akcje')
+    plt.grid(color='gray', linestyle='--')
+    plt.xlabel('nr próbki')
+    plt.ylabel('Ilość pieniędzy lub akcji')
+    plt.title('Symulacja', fontweight='bold')
+    plt.legend()
+    plt.savefig('simulation.png')
+
