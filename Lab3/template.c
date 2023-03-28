48ba6ff0cdb1f0775f8f385633e9c249b8a9a347
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2023-03-28 17:25:53 +0200

    Partially working code added

--- Lab3/template.c
+++ Lab3/template.c
@@ -3,11 +3,13 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <signal.h>
+#include <semaphore.h>
 #include "pqueue.h"
 
 char *filename = "queue.dat";
 
 int itemId = 0;
+sem_t *sem;
 
 typedef struct item item;
 struct item
@@ -34,12 +36,96 @@ void consume(item *it)
 	free(it);
 }
 
+void print_item_con(void *data)
+{
+	item *it = (item *)data;
+	printf("C: Item %d", it->id);
+}
+
+void print_item_prod(void *data)
+{
+	item *it = (item *)data;
+	printf("P: Item %d", it->id);
+}
+
 void producer(pid_t childPid)
 {
+	sem_wait(sem);
+	pqueue *qu = NULL;
+	qunserialize(&qu, sizeof(item), filename);
+	printf("\nUWAGA!\n");
+	qlist(qu, print_item_prod);
+	int size = 0;
+	pqueue *tmp = qu;
+	while (tmp != NULL)
+	{
+		size++;
+		tmp = tmp->next;
+	}
+	if (size > 4)
+	{
+		printf("Producer is waiting for consumer\n");
+		sleep(1);
+	}
+	else
+	{
+		item *it = produce();
+		printf("Producer is producing an item %d\n", it->id);
+		qinsert(&qu, it, it->id);
+		qlist(qu, print_item_prod);
+		qserialize(qu, sizeof(item), filename);
+	}
+	sem_post(sem);
 }
 
 void consumer()
 {
+	sem_wait(sem);
+	pqueue *qu = NULL;
+	qunserialize(&qu, sizeof(item), filename);
+	qlist(qu, print_item_con);
+	pqueue *node = qpop(&qu);
+	qserialize(qu, sizeof(item), filename);
+	sem_post(sem);
+
+	if (node != NULL)
+	{
+		item *it = node->data;
+		printf("Consumer is consuming an item %d\n", it->id);
+		consume(it);
+		free(node);
+	}
+	else
+	{
+		printf("Consumer is waiting for an item\n");
+		sleep(1);
+	}
+}
+
+int main1()
+{
+	pqueue *qu = NULL;
+
+	item *it1 = produce();
+	qinsert(&qu, it1, itemId);
+
+	it1 = produce();
+	qinsert(&qu, it1, itemId);
+
+	it1 = produce();
+	qinsert(&qu, it1, itemId);
+
+	qlist(qu, print_item_con);
+
+	pqueue *node = qpop(&qu);
+	item *data = node->data;
+	printf("\n%d\n", data->id);
+	qserialize(qu, sizeof(item), filename);
+	qlist(qu, print_item_con);
+
+	qunserialize(&qu, sizeof(item), filename);
+
+	qlist(qu, print_item_con);
 }
 
 int main(int argc, char **argv)
@@ -48,8 +134,34 @@ int main(int argc, char **argv)
 	pqueue *qu = NULL;
 	/* watch -n 1 ps -l --forest */
 
+	sem_t temp;
+
+	sem_init(&temp, 1, 1);
+
+	sem = &temp;
+
 	/* create empty queue */
 	qserialize(qu, sizeof(item), filename);
+
+	// setbuf(stdout, NULL);
+
+	pid = fork();
+	if (pid == 0)
+	{
+		while (1)
+		{
+			consumer();
+		}
+	}
+	else
+	{
+		while (1)
+		{
+			producer(pid);
+		}
+	}
+
+	sem_destroy(sem);
 
 	return 0;
 }
