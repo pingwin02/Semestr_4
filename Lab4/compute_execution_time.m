4217a3fbbcdfa27bfaaf802a01bf3baf5300dd2b
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2023-04-13 17:45:38 +0200

    Modified some code

---
+++ Lab4/compute_execution_time.m
@@ -0,0 +1,6 @@
+function [value] = compute_execution_time( N )
+
+value = (N^1.43 + N^1.14) / 1000 - 5000;
+
+end
+
--- Lab4/main_lab4.m
+++ Lab4/main_lab4.m
@@ -3,7 +3,19 @@ clear all
 close all
 
 
-a = 0;   
-b = 50;
+a = 1;   
+b = 60000;
 
-[xvect, xdif, fx, it_cnt] = bisect(a,b,1e-12,@compute_impedance);
+[xvect, xdif, fx, it_cnt] = bisection(@compute_execution_time, a,b,1e-3);
+
+figure
+plot(xvect)
+xlabel("nr iteracji")
+ylabel("wartość")
+title("Wartość przybliżonego rozwiązania funkcji A")
+
+figure
+semilogy(xdif)
+xlabel("nr iteracji")
+ylabel("wartość")
+title("Zmiana wartości przybliżonego rozwiązania funkcji A")
