869f0c3d29427886093002cb775e62be24478112
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2023-04-18 14:39:42 +0200

    Added lab code

--- Lab8/src/main/java/pl/edu/pg/kask/pt/Lab8/App.java
+++ Lab8/src/main/java/pl/edu/pg/kask/pt/Lab8/App.java
@@ -3,64 +3,60 @@ package pl.edu.pg.kask.pt.Lab8;
 import java.util.*;
 
 public class App {
-    private final Queue<Integer> tasks = new LinkedList<>();
-    private final Map<Integer, Integer> results = new HashMap<>();
+    private final Queue<Job> tasks = new LinkedList<>();
+    private final List<Double> results = new LinkedList<>();
     private final List<Thread> threads = new LinkedList<>();
     private final int numThreads;
-    private volatile boolean isRunning = true;
+
+    private int taskID = 0;
 
     public App(int numThreads) {
         this.numThreads = numThreads;
     }
 
-    public synchronized void addTask(int task) {
+    public synchronized void addTask(Job task) {
         tasks.offer(task);
         notifyAll();
     }
 
-    public synchronized Integer getTask() throws InterruptedException {
-        wait();
+    public synchronized Job getTask() throws InterruptedException {
+        while (tasks.isEmpty()) wait();
         return tasks.poll();
     }
 
-    public synchronized void addResult(int task, int result) {
-        results.put(task, result);
+    public synchronized void addResult(double result) {
+        results.add(result);
     }
 
     public void start() {
         for (int i = 0; i < numThreads; i++) {
             Thread thread = new Thread(() -> {
-                while (isRunning) {
+                while (!Thread.interrupted()) {
                     try {
-                        Integer task = getTask();
-                        if (task != null) {
-                            addResult(task, compute(task));
-                        }
+                        Job task = getTask();
+                        compute(task);
                     } catch (InterruptedException e) {
                         Thread.currentThread().interrupt();
-                        return;
                     }
                 }
-                System.out.println(Thread.currentThread().getName() + " finished");
             });
             thread.start();
             threads.add(thread);
         }
 
         Scanner scanner = new Scanner(System.in);
-        while (isRunning) {
+        while (true) {
             System.out.print("Enter a number (or type 'q' to exit): ");
             String input = scanner.nextLine();
             if (input.equals("q")) {
-                isRunning = false;
-                synchronized (this) {
-                    notifyAll();
+                for (Thread thread : threads) {
+                    thread.interrupt();
                 }
                 break;
             }
             try {
-                int task = Integer.parseInt(input);
-                addTask(task);
+                int iter = Integer.parseInt(input);
+                addTask(new Job(taskID++, iter));
             } catch (NumberFormatException e) {
                 System.out.println("Invalid input, please enter an integer or 'q'");
             }
@@ -74,29 +70,11 @@ public class App {
             }
         }
 
-        System.out.println("Results:");
-        for (Map.Entry<Integer, Integer> entry : results.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
-        }
     }
 
-    private Integer compute(int task) throws InterruptedException {
-        //find the first prime number greater than the given number
-        int prime = task + 1;
-        while (!isPrime(prime)) {
-            Thread.sleep(300);
-            prime++;
-        }
-        return prime;
-    }
-
-    private boolean isPrime(int prime) {
-        for (int i = 2; i < prime; i++) {
-            if (prime % i == 0) {
-                return false;
-            }
-        }
-        return true;
+    private void compute(Job task) {
+        System.out.println("\nResult of Task " + task.id + ": " + task.calcPi() + " after " + task.count + " iterations");
+        addResult(task.result);
     }
 
     public static void main(String[] args) {
