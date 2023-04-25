a0f6ebd2d71dbba5085e59fdf51798f2c8470270
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2023-04-25 20:43:33 +0200

    Removed debug lines

--- Proj2/main.c
+++ Proj2/main.c
@@ -1,20 +1,15 @@
-#define _CRTDBG_MAP_ALLOC
-#include <crtdbg.h>
-#ifdef _DEBUG
-#define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
-#define new DEBUG_NEW
-#endif
-
 #include "funkcje.h"
 #include "metody.h"
 
 int main() {
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	srand(time(NULL));
+
 	system("del *.csv");
+
 	zadanieB();
 	zadanieC();
 	zadanieD();
 	zadanieE();
+
 	return 0;
 }
