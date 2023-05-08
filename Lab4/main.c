c129dc475a46718aea6c41cfb4b712c8f42fab26
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2023-05-08 16:56:11 +0200

    Added lab code

--- Lab4/main.c
+++ Lab4/main.c
@@ -4,33 +4,41 @@
 
 void ex1()
 {
-    int count[4][4] = { {0} };
-    double possibilityX[] = { 0.1, 0.2, 0.3, 0.4 };
-    double possibilityY[] = { 0.1, 0.2, 0.3, 0.4 };
+    int count[4][4] = { {0.0} };
+    double possibilities[4][4] = { {0.0, 0.0, 0.4, 0.1}, {0.0, 0.0, 0.0, 0.15}, {0.2, 0.0, 0.0, 0.0}, {0.1, 0.0, 0.05, 0.0} };
+    double possibilityX[4] = { 0.0 };
+    double possibilityY[4][4] = { {0.0} };
+
+    for (int i = 0; i < 4; i++)
+    {
+		double sum = 0;
+        for (int j = 0; j < 4; j++)
+        {
+			sum += possibilities[i][j];
+		}
+		possibilityX[i] = sum;
+	}
 
     for (int i = 0; i < 4; i++)
     {
         for (int j = 0; j < 4; j++)
         {
-            printf("%0.2f\t", possibilityX[i] * possibilityY[j]);
-        }
-        printf("\n");
-    }
-
-    printf("\n");
-
+			possibilityY[i][j] = possibilities[i][j] / possibilityX[i];
+		}
+	}
+      
     for (int i = 0; i < 100000; i++)
     {
         double randomX = (double)rand() / RAND_MAX;
         double randomY = (double)rand() / RAND_MAX;
 
-        int x = 0, y = 0;
+        int x = -1, y = -1;
         double possibility = 0;
 
         for (int j = 0; j < 4; j++)
         {
             possibility += possibilityX[j];
-            if (randomX < possibility)
+            if (randomX < possibility || possibility > 1 - 1e-10)
             {
                 x = j;
                 break;
@@ -40,15 +48,15 @@ void ex1()
         possibility = 0;
         for (int j = 0; j < 4; j++)
         {
-            possibility += possibilityY[j];
-            if (randomY < possibility)
+            possibility += possibilityY[x][j];
+            if (randomY < possibility || possibility > 1 - 1e-10)
             {
                 y = j;
                 break;
             }
         }
 
-        count[y][x]++;
+        count[x][y]++;
     }
 
     for (int i = 0; i < 4; i++)
@@ -59,6 +67,7 @@ void ex1()
         }
         printf("\n");
     }
+
 }
 
 int main()
