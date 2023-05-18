6b366e4e89933cb8a9c8c940281d7c161cdde9a4
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2023-05-19 00:53:47 +0200

    Added 6 lab

---
+++ Lab6/aproksymacjaWielomianowa.m
@@ -0,0 +1,9 @@
+function [x_approx] = aproksymacjaWielomianowa(n, x, N)
+
+mu = [mean(n) std(n)];
+n2 = (n-mu(1))/mu(2);
+wsp_wielomianu = polyfit(n2,x,N);
+x_approx = polyval(wsp_wielomianu,n2);
+
+end
+
